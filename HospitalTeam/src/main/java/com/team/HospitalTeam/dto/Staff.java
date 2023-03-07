9b8a53f38e5e943e68379de00cc62ca214dc38f3
gaganvdeveloper <gaganvdeveloper@gmail.com>
2023-03-07 15:05:52 +0530

    completed Staff Successfully

--- HospitalTeam/src/main/java/com/team/HospitalTeam/dto/Staff.java
+++ HospitalTeam/src/main/java/com/team/HospitalTeam/dto/Staff.java
@@ -3,12 +3,13 @@ package com.team.HospitalTeam.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Staff {
-	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
